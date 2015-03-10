019e273b6c32f1a1442a31166ee83f6dc654494b
Cathryn Griffiths <cathryn.griffiths@gmail.com>
2015-03-10 13:02:42 -0400

    input whitespaces and non-alphanumeric characters are removed now.

--- palindrome.js
+++ palindrome.js
@@ -1,24 +1,27 @@
 function palindromeDetector() {
 	
-	var word = document.getElementById("palindrome").value; 
+	var word = document.getElementById("palindrome").value;
+
+    var newWord = word.replace(/[^A-z0-9]/g,"");
+    newWord= newWord.toLowerCase();
 	
-	function isPalindrome(word) {
-		if (word.length == 1) {
+	function isPalindrome(newWord) {
+		if (newWord.length == 1) {
 			return true; 
 		}
-		if (word.length==2) {
-			return (word.charAt(0)==word.charAt(1)); 
+		if (newWord.length==2) {
+			return (newWord.charAt(0)==newWord.charAt(1));
 		}
 		
-		return (word.charAt(0)==word.charAt(word.length-1) && isPalindrome(word.substring(1,word.length-1)));
+		return (newWord.charAt(0)==newWord.charAt(newWord.length-1) && isPalindrome(newWord.substring(1,newWord.length-1)));
 	}
 
-	if (isPalindrome(word)){
-		document.getElementById("results").innerHTML = "<h2>Results</h2> <p>Your input <blockquote>" + word + "</blockquote> is a palindrome</p>";
+	if (isPalindrome(newWord)){
+		document.getElementById("results").innerHTML = "<h2>Results</h2> <p>Your input <blockquote>" + word + "</blockquote> is a palindrome</p><br />";
 	}
 	else {
-		document.getElementById("results").innerHTML = "<h2>Results</h2> <p>Your input is NOT a palindrome</p>";
-	}
+        document.getElementById("results").innerHTML = "<h2>Results</h2> <p>Your input <blockquote>" + word + "</blockquote> is NOT a palindrome</p><br />";
+    }
 
 	
 
